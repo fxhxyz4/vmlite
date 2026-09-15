577771f24b727d4631e79d0193eb0ec014d48491
fxhxyz4 <fxhxyz8@gmail.com>
2026-09-15 20:24:42 +0300

    feat: replace default print func to printLog with ansi colors support

--- src/utils/parseArgs.cpp
+++ src/utils/parseArgs.cpp
@@ -2,15 +2,20 @@
 #include <stdexcept>
 #include <iostream>
 #include <cstdlib>
+#include <sstream>
 #include "print.h"
 
 using namespace std;
 
 void checkGainWarnings(float percent) {
     if (percent < 0.0f) {
-        cerr << "[WARN] Negative gain (" << percent << "%) will invert the signal phase!\n";
+        ostringstream ss;
+        ss << "Negative gain (" << percent << "%) will invert the signal phase!";
+        printLog(LogLevel::Warn, ss.str());
     } else if (percent > 1000.0f) {
-        cerr << "[WARN] Excessively high gain (" << percent << "%) may cause severe distortion.\n";
+        ostringstream ss;
+        ss << "Excessively high gain (" << percent << "%) may cause severe distortion.";
+        printLog(LogLevel::Warn, ss.str());
     }
 }
 
@@ -18,7 +23,7 @@ Config parseArgs(int argc, char* argv[]) {
     Config config;
 
     if (argc <= 1) {
-        print("[INFO] Please use arguments or type vmlite --help");
+        printLog(LogLevel::Info, "Please use arguments or type vmlite --help");
         exit(1);
     }
 
@@ -37,7 +42,7 @@ Config parseArgs(int argc, char* argv[]) {
 
         if (arg == "--gain" || arg == "-g") {
             if (i + 1 >= argc) {
-                print("[ERROR] Missing percentage value for " + arg);
+                printLog(LogLevel::Error, "Missing percentage value for " + arg);
                 exit(1);
             }
 
@@ -47,19 +52,19 @@ Config parseArgs(int argc, char* argv[]) {
 
                 config.gain = percent / 100.0f;
             } catch (const exception& e) {
-                print("[ERROR] Invalid number format for " + arg);
+                printLog(LogLevel::Error, "Invalid number format for " + arg);
                 exit(1);
             }
             continue;
         }
 
         if (!arg.empty() && arg[0] == '-') {
-            print("[ERROR] Unknown option: " + arg);
-            print("[INFO] Use vmlite --help to see available options.");
+            printLog(LogLevel::Error, "Unknown option: " + arg);
+            printLog(LogLevel::Info, "Use vmlite --help to see available options.");
             exit(1);
         } else {
-            print("[ERROR] Unexpected argument: " + arg);
-            print("[INFO] Use vmlite --help to see available options.");
+            printLog(LogLevel::Error, "Unexpected argument: " + arg);
+            printLog(LogLevel::Info, "Use vmlite --help to see available options.");
             exit(1);
         }
     }
